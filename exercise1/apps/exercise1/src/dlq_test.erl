8a6e2b1ff59a2dab4514e7e0cc8039bcc366140e
Daniel Schruhl <danielschruhl@gmail.com>
2017-03-29 14:38:46 +0200

    Add delete dlq function which does not make sense yet

--- exercise1/apps/exercise1/src/dlq_test.erl
+++ exercise1/apps/exercise1/src/dlq_test.erl
@@ -3,3 +3,6 @@
 
 % Should create an empty DLQ
 initDLQ_test() -> {[], 3, "Test"} = dlq:initDLQ(3, "Test").
+
+% Should create an empty DLQ
+delDLQ_empty_dlq_test() -> ok = dlq:delDLQ(dlq:initDLQ(3, "Test")).
