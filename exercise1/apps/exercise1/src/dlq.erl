3e13765ecb4276d2ca5f2c219e6725081f40ee02
Daniel Schruhl <danielschruhl@gmail.com>
2017-03-29 15:38:20 +0200

    Add simple function to add messages to the dlq

--- exercise1/apps/exercise1/src/dlq.erl
+++ exercise1/apps/exercise1/src/dlq.erl
@@ -6,17 +6,18 @@
 % initialisiert die DLQ mit Kapazität Size. Bei Erfolg wird eine leere DLQ zurück geliefert.
 % Datei kann für ein logging genutzt werden.
 initDLQ(Size,Datei) ->
-    {[],Size,Datei}.
+    dict:new(),
+    [].
 
 delDLQ(Queue) -> ok.
 
 % liefert die Nachrichtennummer, die als nächstes in der DLQ gespeichert werden kann. Bei leerer DLQ ist dies 1.
-expectedNr({[],_,_}) -> 1;
-expectedNr({[[NNr,_,_,_] | _],_,_}) ->  NNr + 1.
+expectedNr([]) -> 1;
+expectedNr([[NNr,_,_,_] | _]) ->  NNr + 1.
 
 % speichert die Nachricht [NNr,Msg,TSclientout,TShbqin] in der DLQ Queue und fügt ihr einen Eingangszeitstempel an
 % (einmal an die Nachricht Msg und als expliziten Zeitstempel TSdlqin mit erlang:now() an die Liste an. Bei Erfolg wird
 % die modifizierte DLQ zurück geliefert. Datei kann für ein logging genutzt werden.
-push2DLQ([NNr,Msg,TSclientout,TShbqin],Queue,Datei) -> ok.
+push2DLQ([NNr,Msg,TSclientout,TShbqin],Queue,Datei) -> [[NNr,Msg,TSclientout,TShbqin, erlang:now()] | Queue].
 
 deliverMSG(MSGNr,ClientPID,Queue,Datei) -> ok.
--- exercise1/apps/exercise1/src/dlq_test.erl
+++ exercise1/apps/exercise1/src/dlq_test.erl
@@ -2,11 +2,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 % Should create an empty DLQ
-initDLQ_test() -> {[], 3, "Test"} = dlq:initDLQ(3, "Test").
+initDLQ_test() -> [] = dlq:initDLQ(3, "Test").
 
 % Should create an empty DLQ
 delDLQ_empty_dlq_test() -> ok = dlq:delDLQ(dlq:initDLQ(3, "Test")).
 
 expectedNr_empty_dlq_test() -> 1 = dlq:expectedNr(dlq:initDLQ(3, "Test")).
-expectedNR_1item_dlq_test() -> 2 = dlq:expectedNr({[[1,"",1337,42]], 3, "Test"}).
-expectedNR_with_one_message_dlq_test() -> 43 = dlq:expectedNr({[[42,"asd",1337,42]], 3, "Test"}).
+expectedNR_1item_dlq_test() -> 2 = dlq:expectedNr([[1,"",1337,42]]).
+expectedNR_with_one_message_dlq_test() -> 43 = dlq:expectedNr([[42,"asd",1337,42]]).
+
+push2DLQ_valid_insertion_in_empty_dlq_test() ->
+     [[3,"foobar",1337,42,Timestamp]] = dlq:push2DLQ([3,"foobar",1337,42],dlq:initDLQ(3, "Test"),"Logfile"),
+    Timestamp > 0.
+push2DLQ_valid_insertion_in_full_dlq_test() ->
+     [[3,"foobar",1337,42,Timestamp]] = dlq:push2DLQ([3,"foobar",1337,42],dlq:initDLQ(3, "Test"),"Logfile"),
+    Timestamp > 0.
