a8ee336a2ab179bcbd84878e10422373711bf07b
Daniel Schruhl <danielschruhl@gmail.com>
2017-03-29 14:09:12 +0200

    Add function to initialize a dlq

--- exercise1/apps/exercise1/src/dlq.erl
+++ exercise1/apps/exercise1/src/dlq.erl
@@ -1,13 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% @author fawin
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 29. Mar 2017 13:34
-%%%-------------------------------------------------------------------
 -module(dlq).
--author("fawin").
 
 %% API
--export([]).
+-export([initDLQ/2, delDLQ/1, expectedNr/1, push2DLQ/3, deliverMSG/4]).
+
+% initialisiert die DLQ mit Kapazität Size. Bei Erfolg wird eine leere DLQ zurück geliefert.
+% Datei kann für ein logging genutzt werden.
+initDLQ(Size,Datei) ->
+    {[],Size,Datei}.
+
+delDLQ(Queue)
+
+expectedNr(Queue)
+
+push2DLQ([NNr,Msg,TSclientout,TShbqin],Queue,Datei)
+
+deliverMSG(MSGNr,ClientPID,Queue,Datei)
