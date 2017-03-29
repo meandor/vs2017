57ba4189aa4c751f2c7c140a9130e9ed7dadadc8
Daniel Schruhl <danielschruhl@gmail.com>
2017-03-29 14:28:01 +0200

    Add more docu to test

--- exercise1/apps/exercise1/src/dlq_test.erl
+++ exercise1/apps/exercise1/src/dlq_test.erl
@@ -1,15 +1,5 @@
-%%%-------------------------------------------------------------------
-%%% @author fawin
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 29. Mar 2017 14:18
-%%%-------------------------------------------------------------------
 -module(dlq_test).
 -include_lib("eunit/include/eunit.hrl").
 
-%% API
--export([]).
-
-init_test() -> {[], 2, "Test"} = dlq:initDLQ(2, "Test").
+% Should create an empty DLQ
+initDLQ_test() -> {[], 3, "Test"} = dlq:initDLQ(3, "Test").
