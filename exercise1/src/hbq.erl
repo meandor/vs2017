c6ff1c131f41784d2b2c682d4625b18b1acdfe1a
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-04 16:20:20 +0200

    Cant compile because of size, please fix @falcowinkler

--- exercise1/src/hbq.erl
+++ exercise1/src/hbq.erl
@@ -12,7 +12,7 @@ initHBQ(Size, Datei) ->
 loop(Messages, HBQSize, CurrentNNr) ->
   receive
     {deliverMSG,NNr,ToClient, Datei} ->
-      dlq:deliverMSG(NNr, ToClient, [[], _Size], Datei);
+      dlq:deliverMSG(NNr, ToClient, [[], 3], Datei); % TODO: fix issue with size
     {dellHBQ} ->
       exit("dellHBQ was called"), ok;
     {pushHBQ, {[NNr, Msg, TSclientout, TShbqin], [DLQ, Size], Datei}}  ->
