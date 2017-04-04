ff9c436913c1a502cf8db9ccb4d68d169fab95a6
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-04 16:19:25 +0200

    Add alternative test syntax with macros

--- exercise1/test/dlq_tests.erl
+++ exercise1/test/dlq_tests.erl
@@ -2,14 +2,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 % Should create an empty DLQ
-initDLQ_test() -> [[], 3] = dlq:initDLQ(3, 'test.log').
+initDLQ_test() -> ?_assert([[], 3] =:= dlq:initDLQ(3, 'test.log')).
 
 % Should delete a DLQ
-delDLQ_empty_dlq_test() -> ok = dlq:delDLQ([[], 3]).
+delDLQ_empty_dlq_test() -> ?_assert(ok =:= dlq:delDLQ([[], 3])).
 
-expectedNr_empty_dlq_test() -> 1 = dlq:expectedNr(dlq:initDLQ(3, 'test.log')).
-expectedNR_1item_dlq_test() -> 2 = dlq:expectedNr([[[1, "", 1337, 42]], 3]).
-expectedNR_with_one_message_dlq_test() -> 43 = dlq:expectedNr([[[42, "asd", 1337, 42]], 3]).
+% Should return the next expected message number
+expectedNr_test_() ->
+  [
+    ?_assert(1 =:= dlq:expectedNr(dlq:initDLQ(3, 'test.log'))), % empty dlq
+    ?_assert(2 =:= dlq:expectedNr([[[1, "", 1337, 42]], 3])), % 1 item in the dlq
+    ?_assert(43 =:= dlq:expectedNr([[[42, "asd", 1337, 42]], 3])) % 1 item in the dlq
+  ].
 
 push2DLQ_valid_insertion_in_empty_dlq_test() ->
   [[[1, "foobar", 1337, 42, Timestamp]], 5] = dlq:push2DLQ([1, "foobar", 1337, 42], dlq:initDLQ(5, 'test.log'), 'test.log'),
