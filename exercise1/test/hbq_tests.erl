4904cb35b96fd52f753b626c2db0f24525364788
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-04 15:43:12 +0200

    Test modules should end with _tests

--- exercise1/test/hbq_tests.erl
+++ exercise1/test/hbq_tests.erl
@@ -1,5 +1,5 @@
+-module(hbq_tests).
 -include_lib("eunit/include/eunit.hrl").
--module(hbq_test).
 
 % Should create an empty DLQ
 initHBQ_test() -> [[], 3] = hbq:initHBQ(3, 'test.log').
--- exercise1/test/reader_client_tests.erl
+++ exercise1/test/reader_client_tests.erl
@@ -1,4 +1,4 @@
--module(reader_client_test).
+-module(reader_client_tests).
 -include_lib("eunit/include/eunit.hrl").
 
 %calculateNewInterval_test() -> 5 = reader_client:calculateNewInterval(6.0).
