b80d5086c3c175d2c7ca24ca1d1ea421cecebb4d
Falco Winkler <falcomail95@yahoo.de>
2017-04-05 05:23:32 +0000

    draft of sendAllConsecutive hb

--- exercise1/src/hbq.erl
+++ exercise1/src/hbq.erl
@@ -6,29 +6,42 @@
 %Wenn die HBQ mehr elemente als 2/3 von Size enthält, wird eine nachricht "aufgefüllt"
 initHBQ(Size, Datei) ->
   werkzeug:logging(Datei, lists:concat(["HBQ>>> initialized with capacity: ", Size, "\n"])),
-  [[], Size], loop([], Size, 0).
+  [[], Size], loop([], Size, 0, dlq:initDLQ(100, "logging")).
 
 
-loop(Messages, HBQSize, CurrentNNr) ->
+loop(Messages, HBQSize, CurrentNNr, DLQ) ->
   receive
     {deliverMSG,NNr,ToClient, Datei} ->
-      dlq:deliverMSG(NNr, ToClient, [lists:nth(Messages, NNr), HBQSize], Datei);
+      dlq:deliverMSG(NNr, ToClient, [Messages, HBQSize], Datei);
     {dellHBQ} ->
       exit("dellHBQ was called"), ok;
     {pushHBQ, {[NNr, Msg, TSclientout, TShbqin], [DLQ, Size], Datei}}  ->
-      lists:append([NNr, Msg, TSclientout, TShbqin], Messages),
+      Messages = lists:append(Messages, [[NNr, Msg, TSclientout, TShbqin]]),
+      Messages = lists:keysort(1, Messages),
       Size = length(Messages),
       if NNr == CurrentNNr ->
-        %TODO Push all messages older than NNr, if consecutive sequence numbers occur
-        dlq:push2DLQ([NNr, Msg, TSclientout, TShbqin], [DLQ, Size], Datei),
-        loop(Messages, HBQSize, CurrentNNr + 1);
+        {CurrentNNr, Messages} = pushAllConsecutiveSequenceNumbers(Messages, DLQ, Datei),
+        loop(Messages, HBQSize, CurrentNNr + 1, DLQ);
         NNr >= CurrentNNr ->
           %We are still waiting for older messages. Just append the message to the holdback queue
-          loop(lists:append([NNr, Msg, TSclientout, TShbqin], Messages), HBQSize, CurrentNNr);
+          loop(lists:append([NNr, Msg, TSclientout, TShbqin], Messages), HBQSize, CurrentNNr, DLQ);
         Size >= HBQSize * (2 / 3) ->
           % TODO: The HBQ is full, insert a placeholder message for the last gap and send all older messages than CurrentNNr
         ok
     end
   end
 .
+
+
+%deliverMSG(MSGNr, ClientPID, Queue, Datei)
+pushAllConsecutiveSequenceNumbers([[NNr, _, _, _] | Tail], DLQ, Datei) ->
+  dlq:push2DLQ(NNr, DLQ, Datei),
+  [NNr2, _, _, _] = Tail,
+  if NNr == NNr2  - 1 ->
+      pushAllConsecutiveSequenceNumbers(Tail, DLQ, Datei)
+  end,
+  {NNr, Tail}
+.
+
+
 
