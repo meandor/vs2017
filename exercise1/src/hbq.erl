66221f5fed49311390be326b02e8bfdb2e21ad48
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-05 17:58:10 +0200

    Add initalization of hbq

--- exercise1/src/hbq.erl
+++ exercise1/src/hbq.erl
@@ -1,46 +1,59 @@
 -module(hbq).
--export([initHBQ/2]).
+-export([start/0, hbq/4]).
 
+loadConfig() ->
+  {ok, Config} = file:consult("./config/server.cfg"),
+  Config.
 
-%Size ist die Größe der DLQ
-%Wenn die HBQ mehr elemente als 2/3 von Size enthält, wird eine nachricht "aufgefüllt"
-initHBQ(Size, Datei) ->
-  werkzeug:logging(Datei, lists:concat(["HBQ>>> initialized with capacity: ", Size, "\n"])),
-  loop([], Size, 0, dlq:initDLQ(100, "DLQLog")), ok.
+log(Config, Message) ->
+  {ok, NodeName} = werkzeug:get_config_value(hbqnode, Config),
+  LogfileName = lists:concat(["HB-DLQ@", NodeName, ".log"]),
+  LogAtom = erlang:list_to_atom(LogfileName),
+  werkzeug:logging(LogAtom, lists:concat(Message)),
+  LogAtom.
 
-
-loop(Messages, HBQSize, CurrentNNr, DLQ) ->
+hbq(HBQ, CurrentNNr, DLQ, Config) ->
   receive
-    {deliverMSG,NNr,ToClient, Datei} ->
-      dlq:deliverMSG(NNr, ToClient, [DLQ, HBQSize], Datei);
-    {dellHBQ} ->
-      exit("dellHBQ was called"), ok;
-    {pushHBQ, {[NNr, Msg, TSclientout, TShbqin], Datei}}  ->
-      werkzeug:logging(Datei, lists:concat(["HBQ>>> pushing message: ", NNr, "\n"])),
-      Messages = lists:append(Messages, [[NNr, Msg, TSclientout, TShbqin]]),
-      Messages = lists:keysort(1, Messages),
-      Size = length(Messages),
-      if NNr == CurrentNNr ->
-        {CurrentNNr, Messages} = pushAllConsecutiveSequenceNumbers(Messages, DLQ, Datei),
-        loop(Messages, HBQSize, CurrentNNr + 1, DLQ);
-        Size >= HBQSize * (2 / 3) ->
-        ok
-    end
+    {ServerPID, {request, initHBQ}} ->
+      Logfile = log(Config, ["HBQ>>> initialized by ", pid_to_list(ServerPID), "\n"]),
+      {ok, DlqLimit} = werkzeug:get_config_value(dlqlimit, Config),
+      ServerPID ! {reply, ok},
+      hbq([], 0, dlq:initDLQ(DlqLimit, Logfile), Config)
+%%    {deliverMSG, NNr, ToClient, Datei} ->
+%%      dlq:deliverMSG(NNr, ToClient, [DLQ, HBQSize], Datei);
+%%    {dellHBQ} ->
+%%      exit("dellHBQ was called"), ok;
+%%    {pushHBQ, {[NNr, Msg, TSclientout, TShbqin], Datei}} ->
+%%      werkzeug:logging(Datei, lists:concat(["HBQ>>> pushing message: ", NNr, "\n"])),
+%%      Messages = lists:append(Messages, [[NNr, Msg, TSclientout, TShbqin]]),
+%%      Messages = lists:keysort(1, Messages),
+%%      Size = length(Messages),
+%%      if NNr == CurrentNNr ->
+%%        {CurrentNNr, Messages} = pushAllConsecutiveSequenceNumbers(Messages, DLQ, Datei),
+%%        loop(Messages, HBQSize, CurrentNNr + 1, DLQ);
+%%        Size >= HBQSize * (2 / 3) ->
+%%          ok
+%%      end
   end
 .
 
 
-%deliverMSG(MSGNr, ClientPID, Queue, Datei)
-pushAllConsecutiveSequenceNumbers([[NNr, MSG, _, _] | Tail], DLQ, Datei) ->
-  dlq:push2DLQ(NNr, DLQ, Datei),
-  [NNr2, _, _, _] = Tail,
-  Fehler = string:str(MSG, "Fehlernachricht"),
-  if NNr == NNr2  - 1 or Fehler ->
-      pushAllConsecutiveSequenceNumbers(Tail, DLQ, Datei)
-  end,
-  werkzeug:logging(Datei, lists:concat(["HBQ>>>sent all messages to dlq until NNR: ", NNr, "\n"])),
-  {NNr, Tail}
-.
+%%%deliverMSG(MSGNr, ClientPID, Queue, Datei)
+%%pushAllConsecutiveSequenceNumbers([[NNr, MSG, _, _] | Tail], DLQ, Datei) ->
+%%  dlq:push2DLQ(NNr, DLQ, Datei),
+%%  [NNr2, _, _, _] = Tail,
+%%  Fehler = string:str(MSG, "Fehlernachricht"),
+%%  if NNr == NNr2 - 1 or Fehler ->
+%%    pushAllConsecutiveSequenceNumbers(Tail, DLQ, Datei)
+%%  end,
+%%  werkzeug:logging(Datei, lists:concat(["HBQ>>>sent all messages to dlq until NNR: ", NNr, "\n"])),
+%%  {NNr, Tail}
+%%.
+
+start() ->
+  Config = loadConfig(),
+  log(Config, ["HBQ>>> server.cfg opened \n"]),
+  spawn(?MODULE, hbq, [[], 0, [], Config]).
 
 
 
--- exercise1/test/hbq_tests.erl
+++ exercise1/test/hbq_tests.erl
@@ -1,25 +1,16 @@
 -module(hbq_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-%full_test_() ->
-%  {foreach, fun setup/0, fun teardown/1,
-%    [{"Assert X", fun initHBQ_test/0},
-%      {"Assert X", fun test_test/0}]}.
+-export([testReplyOKServer/0]).
 
-setup() -> ok.
-teardown(ArgFromSetup) -> ok.
+testReplyOKServer() ->
+  receive
+    {reply, ok} -> ok
+  end.
 
-%initHBQ_test() ->
-%  PID = spawn(hbq, initHBQ, [2,"HBQTestLogging.log"]),
-%  PID ! dellHBQ, ok.
-
-%test_test() ->
-%  PID = spawn(hbq, initHBQ, [ 2,"HBQTestLogging.log"]),
-%  PID ! {pushHBQ , [1, "bla", erlang:now(), erlang:now()],  "HBQTestLogging.log"},
-%  PID ! {pushHBQ , [2, "bla", erlang:now(), erlang:now()],  "HBQTestLogging.log"},
-%  PID ! {pushHBQ , [3, "bla", erlang:now(), erlang:now()],  "HBQTestLogging.log"},
-%  PID ! {pushHBQ , [4, "bla", erlang:now(), erlang:now()],  "HBQTestLogging.log"}.
-
-sort_test() ->
-  Messages = [{11, "Bla"}, {12, "Blubb"}, {1, "Foo"}] ,
-  [{1, "Foo"}, {11, "Bla"}, {12, "Blubb"}] == lists:keysort(1, Messages).
+initHBQ_test() ->
+  ServerPID = spawn(?MODULE, testReplyOKServer, []),
+  HBQPID = hbq:start(),
+  HBQPID ! {ServerPID, {request, initHBQ}},
+  timer:sleep(2000),
+  undefined = erlang:process_info(ServerPID).
