a1866bc5dc6f89f5f79503d3087480447e3cc65f
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-06 22:22:53 +0200

    Add test for getmsgid

--- exercise1/test/server_tests.erl
+++ exercise1/test/server_tests.erl
@@ -16,6 +16,19 @@ terminate_server_after_timeoutseconds_test() ->
   timer:sleep(2000),
   ?assert(undefined =:= whereis(serverPID())).
 
+getmsgid_consecutive_ids_test() ->
+  ClientPID = spawn(?MODULE, testClientGetMSGID, [1]),
+  server:startMe("./test-config/server.cfg"),
+  serverPID() ! {ClientPID, getmsgid},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ClientPID)),
+  ClientPID2 = spawn(?MODULE, testClientGetMSGID, [2]),
+  serverPID() ! {ClientPID2, getmsgid},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ClientPID)),
+  timer:sleep(2000),
+  ?assert(undefined =:= whereis(serverPID())).
+
 getmsgid_no_messages_yet_and_dont_terminate_with_client_requests_test() ->
   ClientPID = spawn(?MODULE, testClientGetMSGID, [1]),
   server:startMe("./test-config/server.cfg"),
