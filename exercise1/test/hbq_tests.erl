b9d24d9ce8aadc12fd318d3f598022d12dd02426
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-07 19:07:03 +0200

    Add deliver MSG test

--- exercise1/test/hbq_tests.erl
+++ exercise1/test/hbq_tests.erl
@@ -27,62 +27,62 @@ initHBQ(ServerPID, Configfile) ->
   HBQPID ! {ServerPID, {request, initHBQ}},
   HBQPID.
 
-%%initHBQ_test() ->
-%%  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
-%%  initHBQ(ServerPID),
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID)).
-%%
-%%push_one_message_into_empty_hbq_and_dlq_test() ->
-%%  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
-%%  ServerPID2 = spawn(?MODULE, testReplyOKServer, [1]),
-%%  HBQPID = initHBQ(ServerPID),
-%%  HBQPID ! {ServerPID, {pushHBQ, [1, "Bla", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID)),
-%%  HBQPID ! {ServerPID2, {request, dellHBQ}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID2)),
-%%  ?assert(undefined =:= erlang:process_info(HBQPID)).
-%%
-%%push_one_invalid_message_into_empty_hbq_test() ->
-%%  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
-%%  ServerPID2 = spawn(?MODULE, testReplyOKServer, [1]),
-%%  HBQPID = initHBQ(ServerPID),
-%%  HBQPID ! {ServerPID, {pushHBQ, [3, "Bla2", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID)),
-%%  HBQPID ! {ServerPID2, {request, dellHBQ}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID2)),
-%%  ?assert(undefined =:= erlang:process_info(HBQPID)).
-%%
-%%push_invalid_messages_into_hbq_test() ->
-%%  ServerPID = spawn(?MODULE, testReplyOKServer, [6]),
-%%  HBQPID = initHBQ(ServerPID, "./test-config/server.cfg"),
-%%
-%%  HBQPID ! {ServerPID, {pushHBQ, [1, "Bla2", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =/= erlang:process_info(ServerPID)),
-%%
-%%  % Insert invalid messages
-%%  HBQPID ! {ServerPID, {pushHBQ, [3, "Bla2", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =/= erlang:process_info(ServerPID)),
-%%  HBQPID ! {ServerPID, {pushHBQ, [4, "Bla2", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =/= erlang:process_info(ServerPID)),
-%%  HBQPID ! {ServerPID, {pushHBQ, [5, "Bla2", erlang:now()]}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =/= erlang:process_info(ServerPID)),
-%%
-%%  HBQPID ! {ServerPID, {request, dellHBQ}},
-%%  timer:sleep(100),
-%%  ?assert(undefined =:= erlang:process_info(HBQPID)),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID)).
+initHBQ_test() ->
+  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
+  initHBQ(ServerPID),
+  timer:sleep(200),
+  ?assert(undefined =:= erlang:process_info(ServerPID)).
+
+push_one_message_into_empty_hbq_and_dlq_test() ->
+  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
+  ServerPID2 = spawn(?MODULE, testReplyOKServer, [1]),
+  HBQPID = initHBQ(ServerPID),
+  HBQPID ! {ServerPID, {pushHBQ, [1, "Bla", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ServerPID)),
+  HBQPID ! {ServerPID2, {request, dellHBQ}},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ServerPID2)),
+  ?assert(undefined =:= erlang:process_info(HBQPID)).
+
+push_one_invalid_message_into_empty_hbq_test() ->
+  ServerPID = spawn(?MODULE, testReplyOKServer, [1]),
+  ServerPID2 = spawn(?MODULE, testReplyOKServer, [1]),
+  HBQPID = initHBQ(ServerPID),
+  HBQPID ! {ServerPID, {pushHBQ, [3, "Bla2", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ServerPID)),
+  HBQPID ! {ServerPID2, {request, dellHBQ}},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(ServerPID2)),
+  ?assert(undefined =:= erlang:process_info(HBQPID)).
+
+push_invalid_messages_into_hbq_test() ->
+  ServerPID = spawn(?MODULE, testReplyOKServer, [6]),
+  HBQPID = initHBQ(ServerPID, "./test-config/server.cfg"),
+
+  HBQPID ! {ServerPID, {pushHBQ, [1, "Bla2", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(ServerPID)),
+
+  % Insert invalid messages
+  HBQPID ! {ServerPID, {pushHBQ, [3, "Bla2", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(ServerPID)),
+  HBQPID ! {ServerPID, {pushHBQ, [4, "Bla2", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(ServerPID)),
+  HBQPID ! {ServerPID, {pushHBQ, [5, "Bla2", erlang:now()]}},
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(ServerPID)),
+
+  HBQPID ! {ServerPID, {request, dellHBQ}},
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(HBQPID)),
+  ?assert(undefined =:= erlang:process_info(ServerPID)).
 
 deliverMSG_test() ->
-  ServerPID = spawn(?MODULE, testReplyMSGNumberServer, [0]),
+  ServerPID = spawn(?MODULE, testReplyMSGNumberServer, [1]),
   ClientPID = spawn(reader, start_reading, [false, 'test.log', [1, 3, 4], ServerPID]),
   HBQPID = initHBQ(ServerPID),
 
@@ -98,25 +98,12 @@ deliverMSG_test() ->
 
   ?assert(undefined =:= erlang:process_info(HBQPID)).
 
-%%fillTest_test() ->
-%%  ServerPID = spawn(?MODULE, testReplyMSGNumber9Server, []),
-%%  HBQPID = initHBQ(ServerPID),
-%%  % HBQPID ! {ServerPID,  {pushHBQ, [0,"Bla",erlang:now()]}},
-%%  HBQPID ! {ServerPID, {pushHBQ, [5, "Bla", erlang:now()]}},
-%%  HBQPID ! {ServerPID, {pushHBQ, [6, "Bla", erlang:now()]}},
-%%  HBQPID ! {ServerPID, {pushHBQ, [7, "Bla", erlang:now()]}},
-%%  HBQPID ! {ServerPID, {pushHBQ, [8, "Bla", erlang:now()]}},
-%%  HBQPID ! {ServerPID, {pushHBQ, [9, "Bla", erlang:now()]}},
-%%  HBQPID ! {ServerPID, {request, deliverMSG, 0, ServerPID}},
-%%  timer:sleep(1000),
-%%  ?assert(undefined =:= erlang:process_info(ServerPID)).
-%%
-%%msglistToTuple_test() ->
-%%  [{3, "Bla", 0, 0}, {1, "Bla", 0, 0}, {2, "Bla", 0, 0}] =
-%%    hbq:apply_on_list([[3, "Bla", 0, 0], [1, "Bla", 0, 0], [2, "Bla", 0, 0]], [], fun list_to_tuple/1).
-%%
-%%sort_hbq_test() ->
-%%  [[1, "Bla", 0, 0], [2, "Bla", 0, 0], [3, "Bla", 0, 0]] = hbq:sort([[3, "Bla", 0, 0], [1, "Bla", 0, 0], [2, "Bla", 0, 0]]).
-%%
-%%sort_hbq_empty_test() ->
-%%  [] = hbq:sort([]).
+msglistToTuple_test() ->
+  [{3, "Bla", 0, 0}, {1, "Bla", 0, 0}, {2, "Bla", 0, 0}] =
+    hbq:apply_on_list([[3, "Bla", 0, 0], [1, "Bla", 0, 0], [2, "Bla", 0, 0]], [], fun list_to_tuple/1).
+
+sort_hbq_test() ->
+  [[1, "Bla", 0, 0], [2, "Bla", 0, 0], [3, "Bla", 0, 0]] = hbq:sort([[3, "Bla", 0, 0], [1, "Bla", 0, 0], [2, "Bla", 0, 0]]).
+
+sort_hbq_empty_test() ->
+  [] = hbq:sort([]).
