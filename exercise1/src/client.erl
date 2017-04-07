0ac2a5681d1e49444d9cab95eed0c9f700af0c60
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-07 19:44:01 +0200

    Add function to start number of clients defined by the config

--- exercise1/src/client.erl
+++ exercise1/src/client.erl
@@ -1,10 +1,24 @@
 -module(client).
--export([start/2]).
 
-start(ServerAddress, Number) -> logger:create(lists:concat([node(), ".log"])), start_n(ServerAddress, Number).
+-export([startClients/0, startClient/1]).
 
-start_n(ServerAddress, 1) -> spawn_client(ServerAddress);
-start_n(ServerAddress, Number) -> spawn_client(ServerAddress), start_n(ServerAddress, Number - 1).
+loadConfig() ->
+  {ok, Config} = file:consult("./config/client.cfg"),
+  Config.
 
-spawn_client(ServerAddress) -> io:format("Spawne client...\n"), spawn(reader_client, setup, [ServerAddress]).
+startClient(Lifetime) ->
+  ok.
 
+do_times(Times, Fn, Arg) -> do_times(Times, Fn, Arg, []).
+
+do_times(0, _Fn, _Arg, Results) -> Results;
+do_times(Times, Fn, Arg, Results) ->
+  NewResults = Results ++ [Fn(Arg)],
+  NewTimes = Times - 1,
+  do_times(NewTimes, Fn, Arg, NewResults).
+
+startClients() ->
+  Config = loadConfig(),
+  {ok, Lifetime} = werkzeug:get_config_value(lifetime, Config),
+  {ok, Clients} = werkzeug:get_config_value(clients, Config),
+  do_times(Clients, fun client:startClient/1, Lifetime).
