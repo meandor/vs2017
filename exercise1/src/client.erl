3566ddd90dc2f34b8974e09cacc646be57e49a8f
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-10 15:17:50 +0200

    client start function only has server node as parameter

--- exercise1/src/client.erl
+++ exercise1/src/client.erl
@@ -7,29 +7,27 @@ loadConfig() ->
   Config.
 
 clientLog() ->
-  erlang:list_to_atom(lists:concat(["Client@", node(), ".log"])).
+  erlang:list_to_atom(lists:concat(["client_nummer@", node(), ".log"])).
 
-% Recursively call first editor then reader clients until timeout occurs
-startClient(Lifetime) ->
+startClient(ServerPID) ->
   Config = loadConfig(),
-  {ok, ServerName} = werkzeug:get_config_value(servername, Config),
+  {ok, Lifetime} = werkzeug:get_config_value(lifetime, Config),
   {ok, SendeIntervall} = werkzeug:get_config_value(sendeintervall, Config),
   % Start editor
-  editor:start_sending(5, clientLog(), [], SendeIntervall, ServerName),
+  ClientPID = erlang:spawn(editor, start_sending, [5, clientLog(), [], SendeIntervall, ServerPID]),
   % Start timeout handler
-  timer:kill_after(Lifetime),
-  self().
+  timer:kill_after(Lifetime, ClientPID),
+  ClientPID.
 
-do_times(Times, Fn, Arg1, Arg2, Arg3) -> do_times(Times, Fn, Arg1, Arg2, Arg3, []).
-do_times(0, _Fn, _Arg1, _Arg2, _Arg3, Results) -> Results;
-do_times(Times, Fn, Arg1, Arg2, Arg3, Results) ->
-  NewResults = Results ++ [Fn(Arg1, Arg2, Arg3)],
+do_times(Times, Fn, Arg) -> do_times(Times, Fn, Arg, []).
+do_times(0, _Fn, _Arg, Results) -> Results;
+do_times(Times, Fn, Arg, Results) ->
+  NewResults = Results ++ [Fn(Arg)],
   NewTimes = Times - 1,
-  do_times(NewTimes, Fn, Arg1, Arg2, Arg3, NewResults).
+  do_times(NewTimes, Fn, Arg, NewResults).
 
 startClients() ->
   Config = loadConfig(),
-  {ok, Lifetime} = werkzeug:get_config_value(lifetime, Config),
   {ok, Clients} = werkzeug:get_config_value(clients, Config),
-  erlang:spawn(?MODULE, startClient, [Lifetime]),
-  do_times(Clients, fun erlang:spawn/3, ?MODULE, startClient, [Lifetime]).
+  {ok, ServerName} = werkzeug:get_config_value(servername, Config),
+  do_times(Clients, fun client:startClient/1, ServerName).
