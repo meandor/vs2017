c8b1c4aab54ae0eddb5150397ee0397a31aa54b4
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-10 16:10:23 +0200

    Add function to generate logfile and message

--- exercise1/src/editor.erl
+++ exercise1/src/editor.erl
@@ -1,5 +1,5 @@
 -module(editor).
--export([start_sending/5, receiveNNr/3, receiveLastNNr/1]).
+-export([start_sending/5, receiveNNr/3, receiveLastNNr/1, createMessage/1]).
 
 calculateNewInterval(Interval) ->
   Increase = werkzeug:bool_rand(),
@@ -11,13 +11,13 @@ calculateNewInterval(Interval) ->
       max(Interval - Value, 1000)
   end.
 
-startLoop(ServerPID, Intervall) ->
-  editor:start(clientLog(), Intervall, ServerPID, self()),
-  receive
-    {doneSending, ReaderNNrs} -> reader:start_reading(false, clientLog(), ReaderNNrs, ServerPID)
-  end,
-  startLoop(ServerPID, calculateNewInterval(Intervall)).
-
+% 0-client@lab18-<0.1313.0>-C-1-03: 22te_Nachricht. Sendezeit: 16.05 18:01:30,769|
+createMessage(NNr) ->
+  {ok, Config} = file:consult("./config/client.cfg"),
+  {ok, Rechnername} = werkzeug:get_config_value(rechnername, Config),
+  {ok, Prgroup} = werkzeug:get_config_value(praktikumsgruppe, Config),
+  {ok, Teamnr} = werkzeug:get_config_value(teamnummer, Config),
+  lists:concat([Rechnername, "-", Prgroup, "-", Teamnr, ": message_number_", integer_to_list(NNr), ". Sent time: ", werkzeug:timeMilliSecond()]).
 
 receiveNNr(ServerPID, ReaderNNrs, Logging) ->
   receive
@@ -36,23 +36,28 @@ receiveLastNNr(Logfile) ->
     {nid, NextNNr} -> werkzeug:logging(Logfile, lists:concat(["EDITOR>>> message number ", NextNNr, " forgotten\n"]))
   end.
 
+% client_<Nummer><Node>.log
+getLogFile([], NextNNr) ->
+  Logfile = erlang:list_to_atom(lists:concat(["client_", NextNNr, node(), ".log"])),
+  werkzeug:logging(Logfile, lists:concat(["client_", NextNNr, node(), "-", pid_to_list(self()), "- Start: ", werkzeug:timeMilliSecond()])),
+  Logfile;
+
+getLogFile(Logfile, _NextNNr) -> Logfile.
+
 start_sending(0, Logfile, ReaderNNRs, SendWait, ServerPID) ->
   ReceiveLastServer = spawn(?MODULE, receiveLastNNr, [Logfile]),
-  werkzeug:logging(Logfile, lists:concat(["EDITOR>>>", " Receive last\n"])),
+  werkzeug:logging(Logfile, lists:concat(["EDITOR>> > ", " Receive last\n"])),
   ServerPID ! {ReceiveLastServer, getmsgid};
-
 
 start_sending(Counter, Logfile, ReaderNNrs, SendWait, ServerPID) ->
   ServerPID ! {self(), getmsgid},
   receive
     {nid, NextNNr} ->
-      werkzeug:logging(Logfile, lists:concat(["EDITOR>>>", "Received NNr", NextNNr, "\n"])),
+      NewLogFile = getLogFile(Logfile, NextNNr),
       NewReaderNNrs = ReaderNNrs ++ [NextNNr],
       %TODO Message format
       wk ! {dropmessage, [NextNNr, "Nachricht vom Editor", erlang:now()]},
       start_sending(Counter - 1, Logfile, NewReaderNNrs, SendWait, ServerPID)
   end,
   timer:sleep(SendWait),
-  werkzeug:logging(Logfile, lists:concat(["EDITOR>>>", Counter, " ID reqeusted\n"])),
-
-.
+  werkzeug:logging(Logfile, lists:concat(["EDITOR>> > ", Counter, " ID reqeusted\n"])).
--- exercise1/test/editor_tests.erl
+++ exercise1/test/editor_tests.erl
@@ -1,7 +1,5 @@
 -module(editor_tests).
 
--export([testClientExpectingMessage/0]).
-
 -include_lib("eunit/include/eunit.hrl").
 
 testClientExpectingMessage() ->
@@ -11,23 +9,26 @@ testClientExpectingMessage() ->
       ok
   end.
 
-simple_test() ->
-
-  {ok, Config} = file:consult("./config/server.cfg"),
-  {ok, ServerName} = werkzeug:get_config_value(servername, Config),
-  ServerPID = server:startMe(),
-  timer:sleep(1000),
-  editor:start("Editor", 100, ServerPID, self()),
-  receive
-    {doneSending, ReaderNNrs} -> io:format("reveiced done"), ok %?assert(ReaderNNrs =:= [1, 2, 3, 4, 5])
-  end,
-  ClientPID = spawn(?MODULE, testClientExpectingMessage, []),
-  ?assert(undefined =/= ClientPID),
-  ServerPID ! {ClientPID, getmessages},
-  timer:sleep(2000),
-  ?assert(undefined =:= erlang:process_info(ClientPID)),
-  ServerPID ! terminate
-.
+message_test() ->
+  ?assert(0 =/= string:rstr(editor:createMessage(132), "lab18-1-3: message_number_132. Sent time:")).
+%%
+%%simple_test() ->
+%%
+%%  {ok, Config} = file:consult("./config/server.cfg"),
+%%  {ok, ServerName} = werkzeug:get_config_value(servername, Config),
+%%  ServerPID = server:startMe(),
+%%  timer:sleep(1000),
+%%  editor:start("Editor", 100, ServerPID, self()),
+%%  receive
+%%    {doneSending, ReaderNNrs} -> io:format("reveiced done"), ok %?assert(ReaderNNrs =:= [1, 2, 3, 4, 5])
+%%  end,
+%%  ClientPID = spawn(?MODULE, testClientExpectingMessage, []),
+%%  ?assert(undefined =/= ClientPID),
+%%  ServerPID ! {ClientPID, getmessages},
+%%  timer:sleep(2000),
+%%  ?assert(undefined =:= erlang:process_info(ClientPID)),
+%%  ServerPID ! terminate
+%%.
 
 
 
