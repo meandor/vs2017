1ef5b3cb336f886bdcc8135cf52a49542677c448
Falco Winkler <falcomail95@yahoo.de>
2017-04-11 12:02:38 +0000

    mocked server test

--- exercise1/test/end_to_end_tests.erl
+++ exercise1/test/end_to_end_tests.erl
@@ -10,25 +10,25 @@ fake_server(Message) ->
   end
 .
 
+waitingTime() -> 1000.
+
 getmessages_with_empty_dlq_and_dont_update_cmem_test() ->
   server:startMe("./test-config/server.cfg"),
   %client:startClient(serverPID(), "Test", [], 2000),
   editor:start_sending(1, "Test", [], 1, wk),
   serverPID() ! {self(), getmessages},
   receive
-    {reply, Message, Terminated} -> ok
-  end,
-  timer:sleep(3000)
+    {reply, [NNr, _Msg | _Timestamps ], Terminated} ->
+      timer:sleep(waitingTime()),
+      ?_assert((NNr =:= 1) and (Terminated)), ok
+  end
 .
 
 fake_server_test() ->
-  MSG = [1, "Test", erlang:now()],
- ServerPID = spawn(?MODULE, fake_server, [MSG]),
-  ServerPID ! {self(), getmessages},
-  receive
-    {reply, Message, Terminated} -> ok
-  end,
-  timer:sleep(3000)
+  MSG = [1, "Test", erlang:now(), erlang:now(),erlang:now(), erlang:now()],
+  ServerPID = spawn(?MODULE, fake_server, [MSG]),
+  {ReaderNNrs, _Logfile} = reader:start_reading(false, "Test", [], ServerPID),
+  ?_assert(ReaderNNrs =:= [1])
 .
 
 
