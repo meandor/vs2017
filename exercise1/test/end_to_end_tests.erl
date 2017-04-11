ffc75287ad2c0a80ebfa10cef89183891da98a71
Falco Winkler <falcomail95@yahoo.de>
2017-04-11 13:47:01 +0000

    minor adaptions

--- exercise1/test/end_to_end_tests.erl
+++ exercise1/test/end_to_end_tests.erl
@@ -27,7 +27,7 @@ editor_start_sending_test() ->
   editor:start_sending(1, "Test", [], 1, wk),
   serverPID() ! {self(), getmessages},
   receive
-    {reply, [NNr, _Msg | _Timestamps ], Terminated} ->
+    {reply, [NNr, _Msg | _Timestamps], Terminated} ->
       timer:sleep(waitingTime()),
       ?assert((NNr =:= 1) and (Terminated))
   end
@@ -35,7 +35,7 @@ editor_start_sending_test() ->
 
 %This tests if 4 messages are read from the server, after they are returned by a mock server
 fake_server_test() ->
-  MSG = [4, "Test", erlang:now(), erlang:now(),erlang:now(), erlang:now()],
+  MSG = [4, "Test", erlang:now(), erlang:now(), erlang:now(), erlang:now()],
   ServerPID = spawn(?MODULE, fake_server, [MSG]),
   {ReaderNNrs, _Logfile} = reader:start_reading(false, "Test", [], ServerPID),
   %io:format("~w~n", [ReaderNNrs]),
@@ -49,28 +49,17 @@ filled_hbq_test() ->
   % forget id 1
   editor:start_sending(0, "Test", [], 1, wk),
   editor:start_sending(5, "Test", [], 1, wk),
-  io:format(lists:concat(["receive_n ", 1,5, "\n"])),
+  io:format(lists:concat(["receive_n ", 1, 5, "\n"])),
   receive_n(1, 6)
 .
 
+%% Empfängt alle nachrichten von N bis Max
 receive_n(Max, Max) -> ok;
 receive_n(N, Max) ->
   io:format(lists:concat(["receive_n ", N, Max, "\n"])),
   serverPID() ! {self(), getmessages},
   receive
-    {reply, [NNr, _Msg | _Timestamps ], false} ->  io:format(lists:concat(["Number received: ", NNr, "\n"])), ?assert((NNr == N))
+    {reply, [NNr, _Msg | _Timestamps], false} -> io:format(lists:concat(["Number received: ", NNr, "\n"])),
+      ?assert((NNr == N))
   end,
   receive_n(N + 1, Max).
-
-
-
-
-%%receive
-%%{reply, [NNr, Msg | _T], Terminated} ->
-%%if NNr =:= -1 ->
-%%ok
-%%end
-%%end,
-%%wk ! terminate,
-%%timer:sleep(3000)
-
