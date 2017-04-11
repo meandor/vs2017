7b868f6037583a768b214571478627cfe883ed80
Falco Winkler <falcomail95@yahoo.de>
2017-04-11 11:49:52 +0000

    two working tests

--- exercise1/test/end_to_end_tests.erl
+++ exercise1/test/end_to_end_tests.erl
@@ -1,25 +1,36 @@
 -module(end_to_end_tests).
-
+-export([fake_server/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 serverPID() -> wk.
 
-fakeClient() ->
+fake_server(Message) ->
   receive
-    {reply, Message, Terminated} -> ok
+    {ClientPID, getmessages} -> ClientPID ! {reply, Message, true}
   end
 .
 
 getmessages_with_empty_dlq_and_dont_update_cmem_test() ->
   server:startMe("./test-config/server.cfg"),
   %client:startClient(serverPID(), "Test", [], 2000),
-  editor:start_sending(1, "Test", [], 2000, wk),
+  editor:start_sending(1, "Test", [], 1, wk),
   serverPID() ! {self(), getmessages},
   receive
     {reply, Message, Terminated} -> ok
-  end
+  end,
+  timer:sleep(3000)
+.
 
- .
+fake_server_test() ->
+  MSG = [1, "Test", erlang:now()],
+ ServerPID = spawn(?MODULE, fake_server, [MSG]),
+  ServerPID ! {self(), getmessages},
+  receive
+    {reply, Message, Terminated} -> ok
+  end,
+  timer:sleep(3000)
+.
+
 
 %%receive
 %%{reply, [NNr, Msg | _T], Terminated} ->
