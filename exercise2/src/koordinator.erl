55613f59f165b48859212ecf8263e26b707e5874
Falco Winkler <falcomail95@yahoo.de>
2017-04-13 15:40:04 +0000

    added koordinator interface stubs. added untested skeleton for starter process. Updated documentation for starter. Added werkzeug utility.

--- exercise2/src/koordinator.erl
+++ exercise2/src/koordinator.erl
@@ -4,4 +4,24 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(koordinator).
--export([]).
+-export([start/1]).
+
+
+start(Config) -> ok.
+
+receive_loop(Config) ->
+  receive
+    {From, getsteeringval} -> ok;
+    {hello,Clientname} -> ok;
+    {briefmi,{Clientname,CMi,CZeit}} -> ok;
+    {From,briefterm,{Clientname,CMi,CZeit}} -> ok;
+    reset -> ok;
+    step -> ok;
+    prompt -> ok;
+    nudge -> ok;
+    toggle -> ok;
+    {calc,WggT} -> ok;
+    kill -> ok
+  end
+.
+
---
+++ exercise2/src/starter.erl
@@ -0,0 +1,59 @@
+%%%-------------------------------------------------------------------
+%%% @doc
+%%% @end
+%%%-------------------------------------------------------------------
+-module(starter).
+
+
+-export([start/1]).
+
+loggingAtom(Config) ->
+  {ok, Name} = werkzeug:get_config_value(koordinatorname, Config),
+  LogfileName = lists:concat(["Koordinator@", Name, ".log"]),
+  erlang:list_to_atom(LogfileName).
+
+loadConfig(Configfile) ->
+  {ok, Config} = file:consult(Configfile),
+  Config.
+
+% Starter hat eindeutige Nummer, Beim starten des Starters wird ihm seine Starternummer mitgegeben.
+start(StarterNumber) -> start(StarterNumber, "./config/ggt.cfg").
+
+start(StarterNumber, ConfigPath) ->
+
+  Config = loadConfig(ConfigPath),
+  Logging = loggingAtom(Config),
+  werkzeug:logging(Logging, lists:concat(["starter>>" , ConfigPath, " opened \n"])),
+
+  %Der Starter liest aus der Datei ggt.cfg die weiteren Werte aus:
+  % die Erlang-Node des Namensdienstes, der Name des Koordinators,
+  % die Nummer der Praktikumsgruppe und die Nummer des Teams.
+
+  {ok, NSNode} = werkzeug:get_config_value(nameservicenode, Config),
+  {ok, NSName} = werkzeug:get_config_value(nameservicename, Config),
+
+  %TODO check if correct?
+  NameService = {NSName, NSNode},
+  net_adm:ping(NSNode),
+  NameService  ! {self(), {rebind, StarterNumber, node()}},
+
+  {ok, CoordinatorName} = werkzeug:get_config_value(koordinatorname, Config),
+  {ok, GroupNumber} = werkzeug:get_config_value(praktikumsgruppe, Config),
+  {ok, TeamNumber} = werkzeug:get_config_value(teamnummer, Config),
+  GroupTeam = erlang:list_to_atom([GroupNumber, TeamNumber]),
+  request_steering_values(Config, CoordinatorName, GroupTeam, StarterNumber).
+
+request_steering_values(Config, CoordinatorName, GroupTeam, StarterNumber) ->
+  CoordinatorName ! {self(), getsteeringval},
+  receive
+    {steeringval,ArbeitsZeit,TermZeit,Quota,GGTProzessnummer} -> startGGT(ArbeitsZeit, TermZeit, Quota, GGTProzessnummer, GroupTeam, StarterNumber)
+  end
+.
+
+startGGT(ArbeitsZeit,TermZeit,Quota,0, GroupTeam, StartNumber) -> ok;
+
+startGGT(ArbeitsZeit,TermZeit,Quota,GGTProzessnummer, GroupTeam, StartNumber) ->
+  %<PraktikumsgruppenID><TeamID><Nummer des ggT-Prozess><Nummer des Starters>
+  GGTProcessName = erlang:list_to_atom([GroupTeam, GGTProzessnummer, StartNumber]),
+  %TODO: StartGGT Process with GGTProccessName and given values
+  startGGT(ArbeitsZeit, TermZeit, Quota, GGTProzessnummer - 1, GroupTeam, StartNumber).
