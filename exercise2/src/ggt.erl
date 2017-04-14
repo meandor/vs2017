ef6b1256160bf5aa5ae271f9667282d55a45d1ff
Falco Winkler <falcomail95@yahoo.de>
2017-04-14 10:23:03 +0000

    finished receive and method stubs

--- exercise2/src/ggt.erl
+++ exercise2/src/ggt.erl
@@ -4,4 +4,24 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(ggt).
--export([]).
+-export([start/4]).
+
+start(WorkingTime,TerminationTime,Quota, GGTName) ->
+  % Der ggT-Prozess meldet sich beim Koordinator mit seinem Namen an (hello) und beim Namensdienst (rebind).
+  % Er registriert sich ebenfalls lokal auf der Erlang-Node mit seinem Namen (register).
+  % Der ggT-Prozess erwartet dann vom Koordinator die Informationen über seine Nachbarn (setneighbors).
+  receive_loop(WorkingTime, TerminationTime, Quota, GGTName).
+
+receive_loop(WorkingTime,TerminationTime,Quota, GGTName) ->
+  receive
+    {setneighbors,LeftN,RightN} -> ok;
+    {setpm,MiNeu} -> ok;
+    {sendy,Y} -> ok;
+    {From,{vote,Initiator}} -> ok;
+    {voteYes,Name} -> ok;
+    {From,tellmi} -> ok;
+    {From,pingGGT} -> From ! {pongGGT,GGTName};
+    kill -> exit("Kill command received"), ok
+  end
+.
+
