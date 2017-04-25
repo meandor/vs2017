30f4a83bb86eee0fb2d058a3b1ae9664ff4210e0
Daniel Schruhl <danielschruhl@gmail.com>
2017-04-25 11:03:58 +0200

    moved test function to end_to_end test

---
+++ exercise2/test/end_to_end_tests.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(end_to_end_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+  werkzeug:ensureNameserviceStarted(),
+  koordinator:start("./test-config/koordinator.cfg"),
+  StarterNumber = list_to_atom(integer_to_list(1)),
+  starter:start(StarterNumber),
+  ClientName = list_to_atom(lists:append(integer_to_list(4881), "1")),
+  ClientName ! {setpm, 3456},
+  ClientName ! {self(), tellmi},
+  receive
+    {mi, Mi} -> ?assert(Mi =:= 3456)
+  end.
--- exercise2/test/starter_tests.erl
+++ exercise2/test/starter_tests.erl
@@ -50,16 +50,3 @@ get_steering_values_test() ->
   timer:sleep(100),
   ?assertEqual(undefined, erlang:process_info(Coordinator)),
   ?assertEqual(undefined, erlang:process_info(StarterPID)).
-
-%%simple_test() ->
-%%  werkzeug:ensureNameserviceStarted(),
-%%  koordinator:start("./test-config/koordinator.cfg"),
-%%  StarterNumber = list_to_atom(integer_to_list(1)),
-%%  starter:start(StarterNumber),
-%%  ClientName = list_to_atom(lists:append(integer_to_list(4881), "1")),
-%%  ClientName ! {setpm, 3456},
-%%  ClientName ! {self(), tellmi},
-%%  receive
-%%    {mi, Mi} -> ?assert(Mi =:= 3456)
-%%  end
-%%.
