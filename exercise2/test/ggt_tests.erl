f49b9f3fa4456ace5b5d188b8e365cc5b98b509b
Falco Winkler <falcomail95@yahoo.de>
2017-05-02 08:04:43 +0000

    enabled set mi test

--- exercise2/test/ggt_tests.erl
+++ exercise2/test/ggt_tests.erl
@@ -47,46 +47,56 @@ start_ggt_process_and_kill_test() ->
   timer:sleep(100),
   ?assertEqual(undefined, erlang:process_info(PID)).
 
-%%
-%%set_mi_test() ->
-%%  ensure_services(),
-%%  start_test_ggt(),
-%%  testProcessName() ! {setpm, 3456},
-%%  testProcessName() ! {self(), tellmi},
-%%  receive
-%%    {mi, Mi} -> ?assert(Mi =:= 3456)
-%%  end,
-%%  kill_ggt().
+
+set_mi_test() ->
+  NameServer = with_redefed_name_service(nameservice),
+  Coordinator = with_redefed_coordinator('1234'),
+  PID = ggt:start(2000, 20000, 6, '4321', Coordinator, NameServer),
+  timer:sleep(100),
+  PID ! {setpm, 3456},
+  PID ! {self(), tellmi},
+  receive
+    {mi, Mi} -> ?assert(Mi =:= 3456)
+  end,
+  PID ! kill,
+  timer:sleep(100),
+  ?assertEqual(undefined, erlang:process_info(PID)).
+
 
 %%set_neigbours_test() ->
 %%  % create a ring
 %%  % dummy(mi = -1) <- left(mi = 9) -><- testProcess(mi = 6) -><- right(mi = 12) -> dummy(mi = -1)
 %%  % if 3 is send to testProcess,
 %%  % it will calculate the reminder 3 and send it to left & right. This should in a ggT of 3 in every process
-%%  ensure_services(),
-%%  start_test_ggt(),
-%%  ggt:start(2000, 20000, 6, left, chef, nameservice),
-%%  ggt:start(2000, 20000, 6, right, chef, nameservice),
-%%  ggt:start(2000, 20000, 6, dummy, chef, nameservice),
+%%
+%%  NameServer = with_redefed_name_service(nameservice),
+%%  Coordinator = with_redefed_coordinator('1234'),
 %%  timer:sleep(100),
-%%  testProcessName() ! {setneighbors, left, right},
-%%  left ! {setpm, 9},
-%%  right ! {setpm, 12},
-%%  left ! {setneighbors, dummy, testProcessName()},
-%%  right ! {setneighbors, testProcessName(), dummy},
-%%  testProcessName() ! {setpm, 6},
-%%  testProcessName() ! {sendy, 3},
+%%  GGTUnderTest = ggt:start(2000, 20000, 6, '4321', Coordinator, NameServer),
+%%  Left = ggt:start(2000, 20000, 6, left, chef, nameservice),
+%%  Right = ggt:start(2000, 20000, 6, right, chef, nameservice),
+%%  Dummy = ggt:start(2000, 20000, 6, dummy, chef, nameservice),
 %%  timer:sleep(100),
-%%  left ! {self(), tellmi},
+%%  GGTUnderTest ! {setneighbors, Left, Right},
+%%  Left ! {setpm, 9},
+%%  Right ! {setpm, 12},
+%%  Left ! {setneighbors, Dummy, GGTUnderTest},
+%%  Right ! {setneighbors, GGTUnderTest, Dummy},
+%%  GGTUnderTest ! {setpm, 6},
+%%  GGTUnderTest ! {sendy, 3},
+%%  timer:sleep(100),
+%%  Left ! {self(), tellmi},
 %%  receive
 %%    {mi, Mi} ->  io:format(lists:concat(["Mi received", Mi])), ?assert(Mi =:= 3)
 %%  end,
-%%  right ! {self(), tellmi},
+%%  Right ! {self(), tellmi},
 %%  receive
-%%    {mi, Mi} ->  io:format(lists:concat(["Mi received", Mi])), ?assert(Mi =:= 3)
+%%    {mi, Mi2} ->  io:format(lists:concat(["Mi received", Mi2])), ?assert(Mi =:= 3)
 %%  end,
-%%  kill_ggt(),
-%%  left ! kill,
-%%  right ! kill,
-%%  dummy ! kill
-%%.
+%%  GGTUnderTest ! kill,
+%%  Left ! kill,
+%%  Right ! kill,
+%%  Dummy ! kill,
+%%  NameServer ! terminate,
+%%  timer:sleep(100),
+%%  ?assertEqual(undefined, erlang:process_info(GGTUnderTest)).
