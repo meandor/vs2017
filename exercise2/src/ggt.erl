fe26214884712ce6b3273faa5ead25e48b03fa21
Daniel Schruhl <danielschruhl@gmail.com>
2017-05-08 00:38:23 +0200

    Add function to start terminate vote process after termtime

--- exercise2/src/ggt.erl
+++ exercise2/src/ggt.erl
@@ -4,7 +4,7 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(ggt).
--export([start/6, start_ggt_process/1, update_mi_state/1, maybe_update_mi/2, update_neighbours/3, set_pm/2]).
+-export([start/6, start_ggt_process/1, reset_terminate_timer/1, maybe_update_mi/2, update_neighbours/3, set_pm/2, term_request/1]).
 
 -spec log(map(), list()) -> atom().
 log(Config, Message) ->
@@ -26,21 +26,25 @@ start_ggt_process(State) ->
   end,
   Coordinator ! {hello, GgTName},
   log(State, ["registered at coordinator"]),
-  handle_messages(State).
+  TermTime = maps:get(termtime, State) * 1000,
+  NewState = maps:update(terminateTimer, timer:apply_after(TermTime, ?MODULE, term_request, [State]), State),
+  handle_messages(NewState).
 
-update_mi_state(State) ->
-  maps:update(lastMiUpdate, erlang:now(), State).
+reset_terminate_timer(State) ->
+  timer:cancel(maps:get(terminateTimer, State)),
+  TermTime = maps:get(termtime, State) * 1000,
+  NewTimer = timer:apply_after(TermTime, ?MODULE, term_request, [State]),
+  maps:update(terminateTimer, NewTimer, State).
 
 update_neighbours(Left, Right, State) ->
   log(State, ["left neighbour registered: ", atom_to_list(Left)]),
   log(State, ["right neighbour registered: ", atom_to_list(Right)]),
-  UpdatedNeighborMaps = maps:update(rightneigbor, Right, maps:update(leftneighbor, Left, State)),
-  update_mi_state(UpdatedNeighborMaps).
+  maps:update(rightneigbor, Right, maps:update(leftneighbor, Left, State)).
 
 set_pm(Mi, State) ->
   log(State, ["setpm: ", integer_to_list(Mi)]),
   NewState = maps:update(mi, Mi, maps:update(isTerminating, false, State)),
-  update_mi_state(NewState).
+  reset_terminate_timer(NewState).
 
 maybe_update_mi(Y, State) ->
   Mi = maps:get(mi, State),
@@ -50,16 +54,26 @@ maybe_update_mi(Y, State) ->
   GgTName = maps:get(ggtname, State),
   if
     Y < Mi ->
-      timer:sleep(maps:get(workingtime, State)),
+      timer:sleep(maps:get(workingtime, State) * 1000),
       NewMi = ((Mi - 1) rem Y) + 1,
       L ! {sendy, NewMi},
       R ! {sendy, NewMi},
       Coordinator ! {briefmi, {GgTName, NewMi, erlang:now()}},
-      log(State, ["sendy: ", integer_to_list(Y), " (", integer_to_list(Mi), "); new mi: ", integer_to_list(NewMi), werkzeug:timeMilliSecond()]),
-      maps:update(mi, NewMi, maps:update(isTerminating, false, State));
+      log(State, ["sendy: ", integer_to_list(Y), " (", integer_to_list(Mi), "); new mi: ", integer_to_list(NewMi), " ", werkzeug:timeMilliSecond()]),
+      NewState = maps:update(mi, NewMi, maps:update(isTerminating, false, State)),
+      reset_terminate_timer(NewState);
     true ->
       log(State, ["sendy: ", integer_to_list(Y), " (", integer_to_list(Mi), "); no new mi"]),
       maps:update(isTerminating, false, State)
+  end.
+
+term_request(State) ->
+  IsTerminating = maps:get(isTerminating, State),
+  if
+    IsTerminating ->
+      ok;
+    true ->
+      maps:get(nameservice, State) ! {self(), {multicast, vote, maps:get(ggtname, State)}}
   end.
 
 handle_messages(State) ->
@@ -96,7 +110,7 @@ start(WorkingTime, TerminationTime, Quota, GgTName, Coordinator, NameService) ->
     rightneigbor => undefined,
     mi => undefined,
     yesVotes => 0,
-    lastMiUpdate => 0,
+    terminateTimer => undefined,
     isTerminating => false
   },
   spawn(?MODULE, start_ggt_process, [State]).
--- exercise2/test/ggt_tests.erl
+++ exercise2/test/ggt_tests.erl
@@ -13,6 +13,8 @@ name_service() ->
   receive
     {From, {rebind, _GgTName, _Node}} ->
       From ! ok;
+    {_From, {multicast, vote, _GgTName}} ->
+      ok;
     terminate -> ok
   end.
 
@@ -42,8 +44,8 @@ with_redefed_neighbour(Mi) ->
 
 simple_state(Coordinator, NameService, LeftN, RightN, Mi) ->
   #{ggtname => 'testggT',
-    workingtime => 3,
-    termtime => 2,
+    workingtime => 1,
+    termtime => 20,
     quota => 2,
     coordinator => Coordinator,
     nameservice => NameService,
@@ -51,7 +53,7 @@ simple_state(Coordinator, NameService, LeftN, RightN, Mi) ->
     rightneigbor => RightN,
     mi => Mi,
     yesVotes => 0,
-    lastMiUpdate => 0,
+    terminateTimer => undefined,
     isTerminating => true}.
 
 start_ggt_process_and_kill_test() ->
@@ -62,9 +64,9 @@ start_ggt_process_and_kill_test() ->
   % Start a ggT process
   PID = ggt:start(2000, 20000, 6, '4321', Coordinator, NameServer),
   timer:sleep(100),
-  ?assertEqual(undefined, erlang:process_info(NameServer)),
+  ?assert(undefined =:= erlang:process_info(NameServer)),
   ?assert(undefined =:= erlang:process_info(Coordinator)),
-  ?assertNotEqual(undefined, erlang:process_info(PID)),
+  ?assert(undefined =/= erlang:process_info(PID)),
 
   % Kill the ggT process
   PID ! kill,
@@ -89,57 +91,14 @@ set_mi_and_tell_mi_test() ->
   ?assert(undefined =:= erlang:process_info(PID)),
   ?assert(undefined =:= erlang:process_info(Coordinator)).
 
-
-%%set_neigbours_test() ->
-%%  % create a ring
-%%  % dummy(mi = -1) <- left(mi = 9) -><- testProcess(mi = 6) -><- right(mi = 12) -> dummy(mi = -1)
-%%  % if 3 is send to testProcess,
-%%  % it will calculate the reminder 3 and send it to left & right. This should in a ggT of 3 in every process
-%%
-%%  NameServer = with_redefed_name_service(nameservice),
-%%  Coordinator = with_redefed_coordinator('1234'),
-%%  timer:sleep(100),
-%%  GGTUnderTest = ggt:start(2000, 20000, 6, '4321', Coordinator, NameServer),
-%%  Left = ggt:start(2000, 20000, 6, left, chef, nameservice),
-%%  Right = ggt:start(2000, 20000, 6, right, chef, nameservice),
-%%  Dummy = ggt:start(2000, 20000, 6, dummy, chef, nameservice),
-%%  timer:sleep(100),
-%%  GGTUnderTest ! {setneighbors, Left, Right},
-%%  Left ! {setpm, 9},
-%%  Right ! {setpm, 12},
-%%  Left ! {setneighbors, Dummy, GGTUnderTest},
-%%  Right ! {setneighbors, GGTUnderTest, Dummy},
-%%  GGTUnderTest ! {setpm, 6},
-%%  GGTUnderTest ! {sendy, 3},
-%%  timer:sleep(100),
-%%  Left ! {self(), tellmi},
-%%  receive
-%%    {mi, Mi} ->  io:format(lists:concat(["Mi received", Mi])), ?assert(Mi =:= 3)
-%%  end,
-%%  Right ! {self(), tellmi},
-%%  receive
-%%    {mi, Mi2} ->  io:format(lists:concat(["Mi received", Mi2])), ?assert(Mi =:= 3)
-%%  end,
-%%  GGTUnderTest ! kill,
-%%  Left ! kill,
-%%  Right ! kill,
-%%  Dummy ! kill,
-%%  NameServer ! terminate,
-%%  timer:sleep(100),
-%%  ?assertEqual(undefined, erlang:process_info(GGTUnderTest)).
-
 set_neighbours_test() ->
   Actual = ggt:update_neighbours('left', 'right', simple_state('undefined', 'nameservice', 'undefined', 'undefined', 0)),
   ?assertEqual('left', maps:get(leftneighbor, Actual)),
   ?assertEqual('right', maps:get(rightneigbor, Actual)).
 
-update_mi_state_test() ->
-  NewState = ggt:update_mi_state(#{lastMiUpdate => 0, ggtname => 'testggT'}),
-  ?assertNotEqual(0, maps:get(lastMiUpdate, NewState)).
-
 set_pm_test() ->
   NewState = ggt:set_pm(123, simple_state('undefined', 'nameservice', 'undefined', 'undefined', 0)),
-  ?assertNotEqual(0, maps:get(lastMiUpdate, NewState)),
+  ?assertNotEqual(0, maps:get(terminateTimer, NewState)),
   ?assertEqual(123, maps:get(mi, NewState)),
   ?assertEqual(false, maps:get(isTerminating, NewState)).
 
@@ -153,7 +112,7 @@ updating_new_mi_test() ->
   ?assert(undefined =/= erlang:process_info(R)),
 
   NewState = ggt:maybe_update_mi(3, simple_state(Coordinator, undefined, L, R, 6)),
-  timer:sleep(100),
+  timer:sleep(1500),
   ?assertEqual(3, maps:get(mi, NewState)),
   ?assertEqual(false, maps:get(isTerminating, NewState)),
   ?assert(undefined =:= erlang:process_info(Coordinator)),
@@ -164,3 +123,18 @@ updating_new_mi_do_nothing_test() ->
   NewState = ggt:maybe_update_mi(6, simple_state(undefined, undefined, undefined, undefined, 6)),
   ?assertEqual(6, maps:get(mi, NewState)),
   ?assertEqual(false, maps:get(isTerminating, NewState)).
+
+term_request_test() ->
+  NameService = with_redefed_name_service(nameservice),
+  State = simple_state(undefined, NameService, undefined, undefined, 0),
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(NameService)),
+
+  ggt:term_request(State),
+  timer:sleep(100),
+  ?assert(undefined =/= erlang:process_info(NameService)),
+
+  NewState = maps:update(isTerminating, false, State),
+  ggt:term_request(NewState),
+  timer:sleep(100),
+  ?assert(undefined =:= erlang:process_info(NameService)).
