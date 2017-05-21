d93817a266b7241fe9cd9599716876a7474e0730
Daniel Schruhl <danielschruhl@gmail.com>
2017-05-21 15:18:27 +0200

    add microservice dependency

--- exercise3/project.clj
+++ exercise3/project.clj
@@ -1,9 +1,10 @@
 (defproject exercise3 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Defines a Station that is sending and receiving messages on a multicast socket via stdma."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [de.otto/tesla-microservice "0.9.2"]]
   :main ^:skip-aot exercise3.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
