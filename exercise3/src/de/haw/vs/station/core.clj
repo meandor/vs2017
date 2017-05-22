babdda59edf574836d7060da683cee8f77e98e4d
Daniel Schruhl <danielschruhl@gmail.com>
2017-05-22 09:16:49 +0200

    Remove components

--- exercise3/src/de/haw/vs/station/core.clj
+++ exercise3/src/de/haw/vs/station/core.clj
@@ -7,13 +7,11 @@
 
 (defn station-system [runtime-config]
   (-> (system/base-system (merge {:name "exercise3"} runtime-config))
-      #_(assoc
-          :product-data-reader (c/using (pdr/new-product-data-reader) [:config :app-status])
-                               :outfit-importer (c/using (oi/new-outfit-importer) [:config :app-status]))
       (httpkit/add-server)))
 
 (defn -main [& args]
   (jvm/instrument-jvm)
+  (prn args)
   (Thread/setDefaultUncaughtExceptionHandler
     (reify Thread$UncaughtExceptionHandler
       (uncaughtException [_ thread ex]
