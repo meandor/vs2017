e8e94697a901ded17b0311aae3ccd529e34b4747
Daniel Schruhl <danielschruhl@gmail.com>
2017-05-25 20:32:22 +0200

    Add finding a free slot for the station

--- exercise3/src/de/haw/vs/business_logic/station.clj
+++ exercise3/src/de/haw/vs/business_logic/station.clj
@@ -1,20 +1,42 @@
 (ns de.haw.vs.business-logic.station
   (:require [com.stuartsierra.component :as c]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [de.haw.vs.data-access.connector :as con]
+            [de.otto.tesla.stateful.app-status :as appstat]
+            [de.otto.status :as stat])
   (:import (java.net MulticastSocket)))
 
-;(defn read [duration slots receiver]
-;  (.setSoTimeout (:socket @(:socket-connection receiver)) (/ duration slots))
-;  (->> (range slots)
-;       (map #(r/read-message receiver))
-;       (filter #(not (nil? %)))))
+(defn find-free-slots [slots messages]
+  (filter (fn [slot] (nil? (some #{slot} (map :slot messages)))) (drop 1 (range (+ 1 slots)))))
 
-(defrecord Station [config app-status receiver]
+(defn read-messages [connector duration slots]
+  (->> (range slots)
+       (map (fn [& _] (con/read-message connector (/ duration slots))))
+       (filter #(not (nil? %)))))
+
+(defn select-free-slot! [state-atom duration slots connector]
+  (->> (read-messages connector duration slots)
+       (find-free-slots slots)
+       (first)
+       (swap! state-atom assoc :slot)))
+
+(defn status [slot-atom]
+  (if (nil? @slot-atom)
+    (stat/status-detail :connector :error "No slot assigned")
+    (stat/status-detail :connector :ok @slot-atom)))
+
+(defrecord Station [config app-status connector]
   c/Lifecycle
   (start [self]
     (log/info "-> starting Station Component")
-    (let [config-params (:config config)])
-    self)
+    (let [config-params (:config config)
+          state-atom (atom {:slot          nil
+                            :station-class (:station-class config-params)
+                            :utc-offset    (:utc-offset config-params)})
+          new-self (assoc self :slot state-atom)]
+      (select-free-slot! state-atom (:frame-size config-params) (:slots-count config-params) connector)
+      (appstat/register-status-fun app-status #(status state-atom))
+      new-self))
 
   (stop [self]
     (log/info "<- stopping Station Component")
---
+++ exercise3/test/de/haw/vs/business_logic/station_test.clj
@@ -0,0 +1,106 @@
+(ns de.haw.vs.business-logic.station-test
+  (:require [clojure.test :refer :all]
+            [de.haw.vs.business-logic.station :as stat]
+            [de.haw.vs.data-access.connector :as con]))
+
+(deftest find-free-slots-test
+  (testing "Should find one free slot"
+    (is (= [1]
+           (stat/find-free-slots 3 [{:station-class   "A"
+                                     :station-name    "foo"
+                                     :payload-content "foobar"
+                                     :payload         "foo foobar"
+                                     :slot            2
+                                     :send-time       (System/currentTimeMillis)}
+                                    {:station-class   "B"
+                                     :station-name    "foo3"
+                                     :payload-content "foobar3"
+                                     :payload         "foo3 foobar3"
+                                     :slot            3
+                                     :send-time       (System/currentTimeMillis)}]))))
+  (testing "Should find two free slot"
+    (is (= [2 4]
+           (stat/find-free-slots 4 [{:station-class   "A"
+                                     :station-name    "foo"
+                                     :payload-content "foobar"
+                                     :payload         "foo foobar"
+                                     :slot            1
+                                     :send-time       (System/currentTimeMillis)}
+                                    {:station-class   "B"
+                                     :station-name    "foo3"
+                                     :payload-content "foobar3"
+                                     :payload         "foo3 foobar3"
+                                     :slot            3
+                                     :send-time       (System/currentTimeMillis)}])))))
+
+(defn test-message [slot]
+  {:station-class   "A"
+   :station-name    (str "station" slot)
+   :payload-content "foobar"
+   :payload         (str "station" slot "foobar")
+   :slot            slot
+   :send-time       slot})
+
+(deftest read-messages-test
+  (let [message-slot (atom 0)]
+    (testing "Should read multiple messages from connector"
+      (with-redefs [con/read-message (fn [connector timeout]
+                                       (swap! message-slot inc)
+                                       (is (= nil connector))
+                                       (is (= 40 timeout))
+                                       (test-message @message-slot))]
+        (is (= [{:station-class   "A"
+                 :station-name    "station1"
+                 :payload-content "foobar"
+                 :payload         "station1foobar"
+                 :slot            1
+                 :send-time       1}
+                {:station-class   "A"
+                 :station-name    "station2"
+                 :payload-content "foobar"
+                 :payload         "station2foobar"
+                 :slot            2
+                 :send-time       2}]
+               (stat/read-messages nil 80 2)))))
+
+    (testing "Should read also empty messages from connector"
+      (reset! message-slot 0)
+      (with-redefs [con/read-message (fn [connector timeout]
+                                       (swap! message-slot inc)
+                                       (is (= nil connector))
+                                       (is (= 40 timeout))
+                                       (when (= @message-slot 2)
+                                         (test-message @message-slot)))]
+        (is (= [{:station-class   "A"
+                 :station-name    "station2"
+                 :payload-content "foobar"
+                 :payload         "station2foobar"
+                 :slot            2
+                 :send-time       2}]
+               (stat/read-messages nil 80 2)))))))
+
+(deftest select-free-slot!-test
+  (let [state-atom (atom {:slot nil})
+        message-slot (atom 0)]
+    (testing "Should select a free slot in the state atom"
+      (with-redefs [con/read-message (fn [connector timeout]
+                                       (swap! message-slot inc)
+                                       (is (= nil connector))
+                                       (is (= 40 timeout))
+                                       (when (not= 1 @message-slot)
+                                         (test-message @message-slot)))]
+        (stat/select-free-slot! state-atom 120 3 nil)
+        (is (= {:slot 1}
+               @state-atom))))
+
+    (testing "Should keep slot empty if no free slot is found"
+      (reset! state-atom {:slot nil})
+      (reset! message-slot 0)
+      (with-redefs [con/read-message (fn [connector timeout]
+                                       (swap! message-slot inc)
+                                       (is (= nil connector))
+                                       (is (= 40 timeout))
+                                       (test-message @message-slot))]
+        (stat/select-free-slot! state-atom 120 3 nil)
+        (is (= {:slot nil}
+               @state-atom))))))
