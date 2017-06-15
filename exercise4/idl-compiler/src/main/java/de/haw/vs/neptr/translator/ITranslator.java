271f30721c25fc3f24fc2cb5d13d1181e0759163
Daniel Schruhl <danielschruhl@gmail.com>
2017-06-15 23:30:08 +0200

    Add javadoc to interface

--- exercise4/idl-compiler/src/main/java/de/haw/vs/neptr/translator/ITranslator.java
+++ exercise4/idl-compiler/src/main/java/de/haw/vs/neptr/translator/ITranslator.java
@@ -3,17 +3,55 @@ package de.haw.vs.neptr.translator;
 import de.haw.vs.neptr.idlmodel.IDLClass;
 import de.haw.vs.neptr.idlmodel.MethodData;
 
+/**
+ * Translator Interface for translating an IDLClass
+ */
 public interface ITranslator {
 
+    /**
+     * Returns translated module declaration of class
+     *
+     * @param idlClass IDLClass to be translated
+     * @return translated module declaration of class
+     */
     public String declareModule(IDLClass idlClass);
 
+    /**
+     * Returns translated opening class declaration of class
+     *
+     * @param idlClass IDLClass to be translated
+     * @return translated opening class declaration of class
+     */
     public String openClassDeclaration(IDLClass idlClass);
 
+    /**
+     * Returns closing class declaration
+     *
+     * @return closing class declaration
+     */
     public String closeClassDeclaration();
 
+    /**
+     * Returns translated narrowCastMethod declaration
+     *
+     * @param idlClass IDLClass to be translated
+     * @return translated narrowCastMethod declaration
+     */
     public String declareNarrowCastMethod(IDLClass idlClass);
 
+    /**
+     * Returns translated methods of class
+     *
+     * @param methodData MethodData to be translated
+     * @return translated methods of class
+     */
     public String declareMethod(MethodData methodData);
 
+    /**
+     * Returns translated filename of class
+     *
+     * @param idlClass IDLClass to be translated
+     * @return translated filename of class
+     */
     public String fileName(IDLClass idlClass);
 }
