3fdf9dce77ac85e7493e41fb11d44f7c14fdd7cd
Falco Winkler <falcomail95@yahoo.de>
2017-06-18 16:15:17 +0000

    Renamed RawObject into ObjectReference

--- exercise4/middleware/src/main/java/mware_lib/ObjectReference.java
+++ exercise4/middleware/src/main/java/mware_lib/ObjectReference.java
@@ -2,14 +2,14 @@ package mware_lib;
 
 import java.io.Serializable;
 
-public class RawObject implements Serializable {
+public class ObjectReference implements Serializable {
 
     private static final long serialVersionUID = -3688393866021967126L;
     private String host;
     private int port;
     private String ref;
 
-    public RawObject(String host, int port, String ref) {
+    public ObjectReference(String host, int port, String ref) {
         this.host = host;
         this.port = port;
         this.ref = ref;
@@ -40,7 +40,7 @@ public class RawObject implements Serializable {
     }
 
     public String toString() {
-        return "RawObject: host(" + host + ") port(" + port + ") ref(" + ref.toString() + ")";
+        return "ObjectReference: host(" + host + ") port(" + port + ") ref(" + ref.toString() + ")";
     }
 
 }
