b5dff796632720fa1b947b72641089b4043f03d6
Falco Winkler <falcomail95@yahoo.de>
2017-06-18 20:59:21 +0000

    end to end tests

--- exercise4/idl-compiler/src/test/java/de/haw/vs/neptr/EndToEndTests.java
+++ exercise4/idl-compiler/src/test/java/de/haw/vs/neptr/EndToEndTests.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EndToEndTests {
 
     private final Path implBaseFile = Paths.get("./src/test/resources/_CalculatorImplBase.java");
+    private final Path proxyFile = Paths.get("./src/test/resources/_Calculator2Proxy.java");
 
     @Before
     public void setUp() throws Exception {
@@ -22,9 +24,11 @@ public class EndToEndTests {
 
     @Test
     public void testWholeSystem() throws Exception {
+        Compiler.main(new String[]{"./src/test/resources/empty.idl", "./src/test/resources/"});
         Compiler.main(new String[]{"./src/test/resources/calc.idl", "./src/test/resources/"});
 
-        assertEquals(true, Files.exists(implBaseFile));
+        assertTrue(Files.exists(implBaseFile));
+        assertTrue(Files.exists(proxyFile));
 
         List<String> expectedContent = new ArrayList<>();
         expectedContent.add("package math_ops;");
@@ -36,6 +40,27 @@ public class EndToEndTests {
         expectedContent.add("\tpublic abstract String getStr(double a) throws Exception;");
         expectedContent.add("}");
 
+
+        List<String> proxyExpectedContent = new ArrayList<>();
+        proxyExpectedContent.add("package math_ops2;");
+        proxyExpectedContent.add("import mware_lib.nameservice.ObjectReference;");
+        proxyExpectedContent.add("import mware_lib.ObjectBroker;");
+        proxyExpectedContent.add("public class _Calculator2Proxy extends _Calculator2ImplBase {");
+        proxyExpectedContent.add("\tprivate ObjectBroker objectBroker;");
+        proxyExpectedContent.add("\tprivate ObjectReference objectReference;");
+        proxyExpectedContent.add("\tpublic _Calculator2Proxy(Object rawReference) {");
+        proxyExpectedContent.add("\t\tObjectReference objectReference = (ObjectReference) rawReference; ");
+        proxyExpectedContent.add("\t\tthis.objectBroker = ObjectBroker.init(objectReference.getHostname(), objectReference.getPort(), false);");
+        proxyExpectedContent.add("\t\tthis.objectReference = objectReference;");
+        proxyExpectedContent.add("\t}");
+        proxyExpectedContent.add("}");
+
         assertEquals(expectedContent, Files.readAllLines(implBaseFile));
+        // Dont care about linebreaks
+        List<String> lines = Files.readAllLines(proxyFile);
+        while (lines.contains(""))
+            lines.remove("");
+        assertEquals(proxyExpectedContent, lines);
+
     }
 }
