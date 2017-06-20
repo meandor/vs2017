4dcaaf27fb80f26286f84a903bc0d9560d83d73b
Falco Winkler <falcomail95@yahoo.de>
2017-06-20 11:10:35 +0000

    passing correct host and port

--- exercise4/middleware/src/main/java/mware_lib/nameservice/NameServiceProxy.java
+++ exercise4/middleware/src/main/java/mware_lib/nameservice/NameServiceProxy.java
@@ -9,12 +9,14 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class NameServiceProxy extends NameService {
 
+    private int serverPort = 9002;
     private String nameServiceHostName;
     private int nameServicePort;
     private ConcurrentMap<String, Object> localRegistry;
@@ -37,8 +39,7 @@ public class NameServiceProxy extends NameService {
         try {
             Socket socket = new Socket(nameServiceHostName, nameServicePort);
             OutputStream out = socket.getOutputStream();
-            // TODO: Pass the host + port of server here
-            ObjectReference ref = new ObjectReference(name, this.nameServiceHostName, this.nameServicePort);
+            ObjectReference ref = new ObjectReference(name, InetAddress.getLocalHost().getHostName(), serverPort);
             byte[] message = NameServiceProtocol.buildRebindMessage(ref, name);
             out.write(message);
             out.flush();
