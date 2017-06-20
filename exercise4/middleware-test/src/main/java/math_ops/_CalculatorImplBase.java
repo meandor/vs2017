48dc7c7d34bfbd17cf52bb55082337ddebc0d215
Falco Winkler <falcomail95@yahoo.de>
2017-06-20 10:40:35 +0000

    deleted middleware-test

--- exercise4/middleware-test/src/main/java/math_ops/_CalculatorImplBase.java
+++
@@ -1,8 +0,0 @@
-package math_ops;
-public abstract class _CalculatorImplBase {
-	public static _CalculatorImplBase narrowCast(Object rawObjectRef) {
-		return new _CalculatorProxy(rawObjectRef);
-	}
-	public abstract double add(double a, double b) throws Exception;
-	public abstract String getStr(double a) throws Exception;
-}
