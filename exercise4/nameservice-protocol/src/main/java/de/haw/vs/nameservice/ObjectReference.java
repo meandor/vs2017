fa0d9349ad44009ca878e868dde18b7d83fca4d3
Falco Winkler <falcomail95@yahoo.de>
2017-06-21 12:10:04 +0000

    added changed middleware to gunter jars

--- exercise4/nameservice-protocol/src/main/java/de/haw/vs/nameservice/ObjectReference.java
+++ exercise4/nameservice-protocol/src/main/java/de/haw/vs/nameservice/ObjectReference.java
@@ -2,58 +2,36 @@ package de.haw.vs.nameservice;
 
 import java.io.Serializable;
 
+
 public class ObjectReference implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1401776112847873409L;
+    private final String refName;
+    private final int port;
+    private final String ip;
 
-    private String alias;
-    private String hostname;
-    private int port;
-    private static final long serialVersionUID = 8233528997584469393L;
-
-
-    public ObjectReference(String alias, String hostname, int port) {
-        this.alias = alias;
-        this.hostname = hostname;
+    public ObjectReference(String refName,String ip, int port) {
+        this.refName = refName;
         this.port = port;
+        this.ip = ip;
     }
 
     public String getAlias() {
-        return alias;
-    }
-
-    public String getHostname() {
-        return hostname;
+        return refName;
     }
 
     public int getPort() {
         return port;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ObjectReference that = (ObjectReference) o;
-
-        if (getPort() != that.getPort()) return false;
-        if (!getAlias().equals(that.getAlias())) return false;
-        return getHostname().equals(that.getHostname());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getAlias().hashCode();
-        result = 31 * result + getHostname().hashCode();
-        result = 31 * result + getPort();
-        return result;
+    public String getHostname() {
+        return ip;
     }
 
     @Override
     public String toString() {
-        return "ObjectReference{" +
-                "alias='" + alias + '\'' +
-                ", hostname='" + hostname + '\'' +
-                ", port=" + port +
-                '}';
+        return refName + "/" + ip + "/" + port;
     }
 }
